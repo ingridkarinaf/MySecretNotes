63620587312b72d048d3856b3857bf1650c40819
ingrid forre <ingridkforre@gmail.com>
2022-11-11 15:16:48 +0100

    wip

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def notes():
             c = db.cursor()
             statement = """INSERT INTO notes(id,assocUser,dateWritten,note,publicID) VALUES(null,?,?,?,?);""" 
             print(statement) # probably not supposed to be here
-            c.execute(statement, (session['userid'],time.strftime('%Y-%m-%d %H:%M:%S'),note,random.randrange(1000000000, 9999999999))
+            c.execute(statement, (session['userid'],time.strftime('%Y-%m-%d %H:%M:%S'),note,random.randrange(1000000000, 9999999999)))
             db.commit()
             db.close()
         elif request.form['submit_button'] == 'import note':
